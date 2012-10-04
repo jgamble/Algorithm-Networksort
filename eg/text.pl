66b398df6c925e9b580afd0501ee8a4ad79f808a
John M. Gamble <jgamble@cpan.org>
2012-10-04 11:18:03 -0500

    Auto-add newline to inputend option, it's a pain otherwise.

--- eg/text.pl
+++ eg/text.pl
@@ -27,6 +27,11 @@ my $inputs = $ARGV[0] || 8;
 
 my @network = nw_comparators($inputs, algorithm => $alg);
 
+#
+# Because it's a pain to do it on the command line, fix the endline.
+#
+$txtset{inputend} .= "\n" if (defined $txtset{inputend});
+
 print nw_graph(\@network, $inputs, graph => 'text', %txtset),
     "\tN= $inputs ", nw_algorithm_name($alg), " Sorting Network\n";
 
